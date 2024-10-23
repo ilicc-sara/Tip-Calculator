150c277705728562662ec877563d2c01046ae725
Sara <sarailic431@gmail.com>
2024-10-23 13:36:32 +0200

    adding functionality to a calculator

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
+"use strict";
 import "./style.css";
 
 const header = document.querySelector("h1");
 
-console.log(header);
+const button = document.querySelector("button");
+console.log(button);
 
-header.addEventListener("click", function () {
-  header.style.color = "red";
+const inputBill = document.querySelector(".bill");
+const inputPercent = document.querySelector(".percentage");
+
+button.addEventListener("click", function () {
+  // header.style.color = "red";
+
+  console.log(inputBill.value);
+  console.log(inputPercent.value);
+
+  const total =
+    inputBill.value * (inputPercent.value / 100) + Number(inputBill.value);
+
+  console.log(total);
+
+  document.querySelector(".total").textContent = total;
 });

b5b6fd913615a218491ea422dc1a9cea5a413b01
Sara <sarailic431@gmail.com>
2024-10-24 12:13:58 +0200

    modify JS

--- main.js
+++ main.js
@@ -9,14 +9,29 @@ console.log(button);
 const inputBill = document.querySelector(".bill");
 const inputPercent = document.querySelector(".percentage");
 
-button.addEventListener("click", function () {
+const form = document.querySelector(".container");
+
+let bill;
+let tipPercent;
+
+inputBill.addEventListener("input", function (e) {
+  bill = e.target.value;
+  console.log(bill);
+});
+
+inputPercent.addEventListener("input", function (e) {
+  tipPercent = e.target.value;
+  console.log(tipPercent);
+});
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
   // header.style.color = "red";
 
   console.log(inputBill.value);
   console.log(inputPercent.value);
 
-  const total =
-    inputBill.value * (inputPercent.value / 100) + Number(inputBill.value);
+  const total = bill * (tipPercent / 100) + Number(bill);
 
   // console.log(total);
 
